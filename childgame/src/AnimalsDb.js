023d7bbed1866bd7cd2527f5f004d00a632569e2
Inshafa-Kaleel <154611829+Inshafa-Kaleel@users.noreply.github.com>
2024-11-24 17:34:37 +0000

    add Animals data

---
+++ childgame/src/AnimalsDb.js
@@ -0,0 +1,20 @@
+export const animals=[
+    {name:'dog',img:'/img/1.jpg'},
+    {name:'rabbit',img:'/img/2.jpg'},
+    {name:'tiger',img:'/img/3.jpg'},
+    {name:'giraffe',img:'/img/4.jpg'},
+    {name:'lion',img:'/img/5.jpg'},
+    {name:'panda',img:'/img/6.jpg'},
+    {name:'owl',img:'/img/7.jpg'},
+    {name:'elephant',img:'/img/8.jpg'},
+    {name:'cat',img:'/img/9.jpg'},
+    {name:'sheep',img:'/img/10.jpg'},
+    {name:'donkey',img:'/img/11.jpg'},
+    {name:'fox',img:'/img/12.jpg'},
+    {name:'wolf',img:'/img/13.jpg'},
+    {name:'bear',img:'/img/14.jpg'},
+    {name:'hen',img:'/img/15.jpg'},
+    {name:'deer',img:'/img/16.jpg'}
+];
+
+export default animals;
